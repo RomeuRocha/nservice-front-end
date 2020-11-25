a3cb953b9960c517566eb8d87c0d0011b56c3953
Anderson Alves <anderson96627210@gmail.com>
2020-11-25 18:03:03 -0300

    menu

--- src/components/Layout/MyMenu.js
+++ src/components/Layout/MyMenu.js
@@ -1,31 +1,42 @@
 import * as React from 'react';
-import { createElement } from 'react';
 import { useSelector } from 'react-redux';
 import { useMediaQuery } from '@material-ui/core';
 import { MenuItemLink, getResources } from 'react-admin';
 import DefaultIcon from '@material-ui/icons/ViewList';
-import LabelIcon from '@material-ui/icons/Label';
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Collapse from '@material-ui/core/Collapse';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+
 import ListAltIcon from '@material-ui/icons/ListAlt';
+import StorageIcon from '@material-ui/icons/Storage';
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
+import ListIcon from '@material-ui/icons/List';
+import WorkIcon from '@material-ui/icons/Work';
+import SubjectIcon from '@material-ui/icons/Subject';
+import NoSimIcon from '@material-ui/icons/NoSim';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import GroupIcon from '@material-ui/icons/Group';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
     button: {
         border: "none",
         marginLeft: 5,
         backgroundColor: "red",
-        
+        paddingLeft: 10,
     },
-   
+    listInterna: {
+        backgroundColor: "transparent",
+        marginLeft: 20,
+    },
+
 }));
 
 const Menu = ({ onMenuClick, logout }) => {
@@ -34,6 +45,7 @@ const Menu = ({ onMenuClick, logout }) => {
     const resources = useSelector(getResources);
 
     const [open, setOpen] = React.useState(true);
+    const [open1, setOpen1] = React.useState(true);
 
     const classes = useStyles();
 
@@ -41,10 +53,13 @@ const Menu = ({ onMenuClick, logout }) => {
     const handleClick = () => {
         setOpen(!open);
     };
+    const handleClick1 = () => {
+        setOpen1(!open1);
+    };
 
     return (
         <div>
-            {resources.map(resource => (
+          {/*  {resources.map(resource => (
                 <MenuItemLink
                     key={resource.name}
                     to={`/${resource.name}`}
@@ -58,22 +73,47 @@ const Menu = ({ onMenuClick, logout }) => {
                     onClick={onMenuClick}
                     sidebarIsOpen={open}
                 />
-            ))}
+            ))} */}
 
             <List component="nav" >
-                <ListItem button onClick={handleClick} className={classes.button}>
-                    <ListAltIcon/>
-                    <Button>Adesao</Button>    
+                
+                <MenuItemLink to="/funcionario" primaryText="Funcionários" leftIcon={<AssignmentIndIcon/>}/>
+                
+                <ListItem button onClick={handleClick} >
+                    <AssignmentIcon />
+                    <MenuItem>Ordem de Serviços</MenuItem>
                     {open ? <ExpandLess /> : <ExpandMore />}
                 </ListItem>
 
                 <Collapse in={open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                        <MenuItemLink to="/adesao" primaryText="API-ENDPOINT-1" />
-                        <MenuItemLink to="/your-api-endpoint-2" primaryText="API-ENDPOINT-2" />
-                        <MenuItemLink to="/your-api-endpoint-3" primaryText="API-ENDPOINT-3" />
+                    <List component="div" disablePadding className={classes.listInterna}>
+                        <MenuItemLink to="/ordemservico/create" primaryText="Nova" leftIcon={<ControlPointIcon/>} />
+                        <MenuItemLink to="/ordemservico" primaryText="Lista se OS" leftIcon={<ListIcon/>} />
+                        <MenuItemLink to="/servico" primaryText="Serviços" leftIcon={<WorkIcon/>}/>
+                        <MenuItemLink to="/assunto" primaryText="Assuntos" leftIcon={<SubjectIcon/>}/>
                     </List>
                 </Collapse>
+
+                
+                <MenuItemLink to="/cliente" primaryText="Clientes" leftIcon={<GroupIcon/>}/>
+                
+          
+                <ListItem button onClick={handleClick1} >
+                    <PostAddIcon />
+                    <MenuItem>Adesao</MenuItem>
+                    {open1 ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+
+                <Collapse in={open1} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding className={classes.listInterna}>
+                        <MenuItemLink to="/adesao/create" primaryText="Nova Adesão" leftIcon={<ControlPointIcon/>}/>
+                        <MenuItemLink to="/adesao" primaryText="Lista de Adesões" leftIcon={<ListIcon/>}/>
+                        <MenuItemLink to="/plano" primaryText="Planos" leftIcon={<EventNoteIcon/>}/>
+                        <MenuItemLink to="/cancelamento" primaryText="Cancelamentos" leftIcon={<NoSimIcon/>}/>
+                    </List>
+                </Collapse>
+
+
             </List>
 
             {isXSmall && logout}
